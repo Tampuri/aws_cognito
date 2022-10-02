53860d401eaaeef61ec519a039a85baab5623fa1
Trevor Kennedy <15678562+Tampuri@users.noreply.github.com>
2022-10-02 20:15:35 +1300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_DIMepBULG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'p2b0b3s8gmnnmgpimg5a5f8lg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'ap-southeast-2:225242124084:userpool/ap-southeast-2_DIMepBULG' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
